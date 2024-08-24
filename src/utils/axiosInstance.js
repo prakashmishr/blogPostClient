85f5829a87d2b83bfa24f06ca31b0edecd624a5a
prakashmishra <rajaprakash4672@gmail.com>
2024-08-24 13:02:47 +0530

    env file added

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:   "https://blogpostserver.onrender.com", 
+  baseURL:   process.env.REACT_APP_SERVER_URL, 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
